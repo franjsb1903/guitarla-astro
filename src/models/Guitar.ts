d98b7a4727d4340defd975d7274bbb3617547bd6
Fran Saa <franchu.besteiro@gmail.com>
2023-01-15 14:49:38 +0100

    Init project, creating routes, nosotros page and fetch guitars

---
+++ src/models/Guitar.ts
@@ -0,0 +1,20 @@
+export interface Guitar {
+  name: string
+  createdAt: Date
+  updatedAt: Date
+  publishedAt: Date
+  description: string
+  price: number
+  url: string
+  image: {
+    data: {
+        attributes: {
+            formats: {
+                medium: {
+                    url: string
+                }
+            }
+        }
+    }
+  }
+}
